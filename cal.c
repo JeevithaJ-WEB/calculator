8c3fc4eb83fd1f9f83e3924ed969aad57a53041c
JeevithaJ-WEB <jeevi2732003@gmail.com>
2024-11-04 16:59:08 +0530

    Create cal.c
    main function cal is added

---
+++ cal.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    float n1,n2;
+    printf("Enter the numbers:");
+    scanf("%f %f",&n1,&n2);
+    int op;
+   printf("enter 1 for addition \n 2 for subtraction \n 3 for multiplication \n 4 for divivsion\n");
+    scanf("%d",&op);
+    
+
+   return 0;
+}
